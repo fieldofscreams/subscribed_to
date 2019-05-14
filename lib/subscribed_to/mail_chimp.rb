3c821fe249fa21379f7928286cbe6dfefe162391
Kanyon Bollinger <kanyonb38@gmail.com>
2019-05-14 15:18:01 -0400

    4.2 - Fixed subscriber issue

--- lib/subscribed_to/mail_chimp.rb
+++ lib/subscribed_to/mail_chimp.rb
@@ -25,10 +25,22 @@ module SubscribedTo
         merge_vars = self.class.merge_vars.dup
 
         if subscribed_to_list
-          g = Gibbon::Request.new(api_key: SubscribedTo.mail_chimp_config.api_key)
+          
+          subscribed_email = self.email
+          lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(subscribed_email.downcase)
+          list_id          = self.class.list_id
+          g                = Gibbon::Request.new(api_key: SubscribedTo.mail_chimp_config.api_key)
+          
+          if !self.first_name.nil? && !self.last_name.nil?
+            
+            g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
+            
+          else
+            g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed"})
+             
+          end
+          
         end
-      rescue Timeout::Error, Gibbon::MailChimpError => e
-        Rails.logger.warn e
       end
 
       # Update attributes of existing member
@@ -41,8 +53,7 @@ module SubscribedTo
         if !(self.changed & merge_vars.collect { |key, method| method.to_s }.push("subscribed_to_list")).empty?
           api_key          = config.api_key
           list_id          = self.class.list_id
-          email_attr       = merge_vars["EMAIL"]
-          subscribed_email = self.changed.include?(email_attr.to_s) ? changed_attributes[email_attr.to_s] : self.send(email_attr)
+          subscribed_email = self.email
           g                = Gibbon::Request.new(api_key: api_key)
 
           if self.changed.include?("subscribed_to_list")
@@ -51,23 +62,18 @@ module SubscribedTo
             if !subscribed_to_list
               g.lists(list_id).members(lower_case_md5_hashed_email_address).update(body: { status: "unsubscribed" })
             else
-              
-              begin
-                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed", merge_fields: merge_vars.each { |key, method| merge_vars[key] = self.send(method.to_sym) }})
-              rescue Gibbon::MailChimpError => e
-                # User is already on the list and unsubscribed from the mailing list. Set to pending so they can resubscribe
-                if e.title == "Member In Compliance State"
-                  g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "pending" })
-                end
+            
+              if !self.first_name.nil? && !self.last_name.nil?
+                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
+              else
+                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed"})
               end
               
-              
+            end
+
             end
           end
         end
-      rescue Gibbon::MailChimpError => e
-        Rails.logger.warn e
-      end
     end
   end
 end
