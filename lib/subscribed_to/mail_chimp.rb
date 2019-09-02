4370ca57983337537a7e5c3f1903923516cf2103
Kanyon Bollinger <kanyonb38@gmail.com>
2019-09-02 14:04:05 -0400

    4.2 - Add Begin Rescue to all options. Don't fail workflow if email is fake or invalid

--- lib/subscribed_to/mail_chimp.rb
+++ lib/subscribed_to/mail_chimp.rb
@@ -32,15 +32,19 @@ module SubscribedTo
           g                = Gibbon::Request.new(api_key: SubscribedTo.mail_chimp_config.api_key)
           
           if !self.first_name.nil? && !self.last_name.nil?
-            
-            g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
-            
+            begin
+              g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
+            rescue Gibbon::MailChimpError => ex
+              if ex.title.to_s.include? "Compliance" # This user unsubscribed from us in the past, but wants to resubscribe
+                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "pending", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
+              end
+            end
           else
             begin
               g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed"})
             rescue Gibbon::MailChimpError => ex
               if ex.title.to_s.include? "Compliance" # This user unsubscribed from us in the past, but wants to resubscribe
-                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "pending", merge_fields: {FNAME: self.first_name, LNAME: self.last_name}})
+                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "pending"})
               end
             end
              
@@ -78,7 +82,13 @@ module SubscribedTo
                   end
                 end
               else
-                g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed"})
+                begin
+                  g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "subscribed"})
+                rescue Gibbon::MailChimpError => ex
+                  if ex.title.to_s.include? "Compliance" # This user unsubscribed from us in the past, but wants to resubscribe
+                    g.lists(list_id).members(lower_case_md5_hashed_email_address).upsert(body: {email_address: subscribed_email, status: "pending"})
+                  end
+                end
               end
               
             end
